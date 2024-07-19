081a1fc4b3e64aee0e42f8df30f7c274ed42b3d5
Barasa-Micah <barasamica@gmail.com>
2024-07-19 15:17:34 +0300

    Added logo

---
+++ src/components/LoginPage.js
@@ -0,0 +1,12 @@
+import React from React;
+
+
+const LoginPage = () => {
+    return(
+        <div className="wrapper">
+            <div className="login-container">
+                <img src={} alt="" className="logo"/>
+            </div>
+        </div>
+    )
+}
