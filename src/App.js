13c01e979229959a042ede1a4e210036002ee294
Barasa-Micah <barasamica@gmail.com>
2024-07-19 20:49:15 +0300

    Changed the hivering colors

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
 import SignupPage from './components/SignupPage';
 import PasswordRecoveryPage from './components/PasswordRecoverypage';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<LoginPage />} />
         <Route path='/signup'  element={<SignupPage />} />
         <Route path='/recover' element={<PasswordRecoveryPage />} />
+        <Route path='/dashboard' element={<Dashboard />} />
       </Routes>
     </Router>
   );
