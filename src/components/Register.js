c42f28a6faa5abe5aa2d6665cc7e1b8ea36932e9
Barasa-Mica <barasamica@gmail.com>
2024-07-29 23:44:16 +0300

    Created the register form

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import logo from '../assets/luxurylogo.jpeg';
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -23,5 +24,64 @@ const Register = () => {
   };
 
 
-  
+  return(
+    <div className="RegisterContainer">
+        <form className="RegisterForm" onSubmit={handleSubmit}>
+          <img src={logo} alt="Luxury Life Adverts" className="logo" />
+            <h2>Register</h2>
+            <input 
+            type="text"
+            name="username"
+            placeholder="formData.username"
+            onChange={handleChange}
+            required
+            />
+            <input 
+            type="email"
+            name="email"
+            placeholder="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+            />
+            <select 
+            name="country"
+            value={formData.country}
+            onChange={handleChange}
+            required
+            >
+              <option value="">-- Choose Country --</option>
+              <option value="Kenya">Kenya</option>
+              <option value="Uganda">Uganda</option>
+              <option value="Tanzania">Tanzania</option>
+            </select>
+            <input 
+            type="tel"
+            name="phone"
+            placeholder="Phone"
+            value={formData.phone}
+            onChange={handleChange}
+            required
+            />
+            <input 
+            type="password"
+            name="confirmPassword"
+            placeholder="confirmPassword"
+            value={formData.confirmPassword}
+            onChange={handleChange}
+            required
+            />
+            <div> Your Upline: {formData.upline} </div>
+            <button type="submit">Create Account</button>
+            <div>
+              <a href="/login">Already have an Account</a>
+              <a href="/customer-care">Customer Care</a>
+            </div>
+        </form>
+        <div className="RegisterImage"></div>
+    </div>
+  )
 };
+
+
+export default Register
