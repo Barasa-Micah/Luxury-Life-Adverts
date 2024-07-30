8be7a763d064ffcefcbaadf76a12c6a7e01f7bfc
Barasa-Mica <barasamica@gmail.com>
2024-07-30 09:03:41 +0300

    Used Routes instead os Switch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Register from './components/Register';
+import Login from './components/Login';
 
 function App() {
   return (
-    <div className="App">
-      <Register />
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path='/register' Component={Register} />
+          <Route path='/login' Component={Login} />
+          <Route path='/' Component={Login} />
+        </Routes>
     </div>
+    </Router>
   );
 }
 
